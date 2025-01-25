b74b04d45390eaa4f79198d48d28763f8d137327
SkY665 <160379145+SkY665@users.noreply.github.com>
2025-01-24 21:37:13 -0500

    Update gestor_tareas.sql

--- gestor_tareas.sql
+++ gestor_tareas.sql
@@ -20,7 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestor_tareas`
 --
+CREATE DATABASE gestor_tareas;
 
+USE gestor_tareas;
 -- --------------------------------------------------------
 
 --
